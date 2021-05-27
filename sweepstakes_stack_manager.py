df3fd93b6db3f28e4e807987b87a7c9462844f4f
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-27 15:50:24 -0400

    Completed Stack Manager methods.

--- sweepstakes_stack_manager.py
+++ sweepstakes_stack_manager.py
@@ -4,4 +4,10 @@ from stack import Stack
 class SweepStakesManager:
 
     def __init__(self):
-        self.stack = Stack
+        self.stack = Stack()
+
+    def insert_sweepstakes(self, sweepstakes):
+        self.stack.push(sweepstakes)
+
+    def get_sweepstakes(self):
+        self.stack.pop()
