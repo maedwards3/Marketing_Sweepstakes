d256b669c6666959294a8941a252b8ad23dc2595
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-27 15:49:34 -0400

    Completed Queue Manager methods.

--- sweepstakes_queue_manager.py
+++ sweepstakes_queue_manager.py
@@ -1,7 +1,13 @@
-from queue import Queue
+from data_queue import Queue
 
 
 class SweepstakesQueueManager:
 
     def __init__(self):
-        self.queue = Queue
+        self.queue = Queue()
+
+    def insert_sweepstakes(self, sweepstakes):
+        self.queue.enqueue(sweepstakes)
+
+    def get_sweepstakes(self):
+        self.queue.dequeue()
