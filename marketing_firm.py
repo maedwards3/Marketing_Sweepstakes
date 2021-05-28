7baec5c166c8a5cde2295f3f20176dd6c8d46e62
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-27 21:29:04 -0400

    Restructured MarketingFirm class. Added comments to give guidance on further work.

--- marketing_firm.py
+++ marketing_firm.py
@@ -1,4 +1,15 @@
+# import user_interface
+from sweepstake import Sweepstake
+
+
 class MarketingFirm:
 
-    def __init__(self, manager):
+    def __init__(self, manager, name):
         self.manager = manager
+        self.name = name
+
+    # not entirely sure how to create
+    def create_sweepstakes(self):
+        sweepstakes_name = input("Enter the name of your sweepstakes: ")
+        sweepstakes = Sweepstake(sweepstakes_name)
+        sweepstakes.contestant_list = {}
