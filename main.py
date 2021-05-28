5d6242d13a94b013efd430718caddd3afadfdc70
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-27 21:33:32 -0400

    Commented-out beginning method calls. Instantiated single method to run simulation.

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+import user_interface
+# from contestant import Contestant
+# from sweepstake import Sweepstake
 
 
 def run_simulation():
-    pass
+    user_interface.main_menu()
+
+
+run_simulation()
+
+# all code below was used in testing bare bones original code. Not required in current implementation
+# ...keeping for future testing
+
+# contestant1 = Contestant()
+# contestant1.first_name = "Mike"
+# contestant1.last_name = "Edwards"
+# contestant1.email = "mike.edwards@gmail.com"
+#
+# contestant2 = Contestant()
+# contestant2.first_name = "Sally"
+# contestant2.last_name = "Baker"
+# contestant2.email = "sally.baker@gmail.com"
+#
+# contestant3 = Contestant()
+# contestant3.first_name = "Rob"
+# contestant3.last_name = "Smith"
+# contestant3.email = "rob.smith@gmail.com"
+#
+# sweepstake_list = Sweepstake("Test")
+# sweepstake_list.register_contestant(contestant1)
+# sweepstake_list.register_contestant(contestant2)
+# sweepstake_list.register_contestant(contestant3)
+# sweepstake_list.print_all_contestants()
+# print()
+# sweepstake_list.pick_winner()
+# sweepstake_list.print_contestant_info()
+
